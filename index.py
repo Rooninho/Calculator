f9a9e44af015b619ccc8f013230665dafeb63f47
Rooninho <rooneywandes3@gmail.com>
2025-08-22 13:21:20 +0300

    changed calculator.py

---
+++ index.py
@@ -0,0 +1,30 @@
+# name=str(input('enter your name:'))
+# print(f'Hello,{name} welcome to calculator program')
+
+# name='Rooninho'
+
+
+# age=19
+
+
+
+# a=5
+# b=10
+
+# print('sum:', a+b)
+
+# list
+# student_id=[11299,
+#             11298,
+#             11297]
+
+# print(student_id[2])
+# student_id.append(11296)
+# print(student_id)
+
+
+# salamu="hello world"
+# for letter in salamu:
+#    print(letter)
+
+
---
+++ quiz.py
@@ -0,0 +1,13 @@
+import random
+import sys
+
+quiz_bank={
+    "python" :{
+        "q": "what is the output of print(type([]))?",
+        "choices": ["< class 'dict' >", "< class 'list' >","< class 'turple' > " "< class 'set' >"],
+        "answer" : 1,
+        
+
+
+    }
+}
